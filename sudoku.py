41e0f7b17b6426f262de943366c3b13d1bb5e7e0
Luca Matarelli <luca.matarelli@gmail.com>
2022-05-10 16:07:17 +0200

    SudokuGrid.__init__() : première implémentation du processus du préremplissage randomisé de la grille + renommage .insert(coords,value) en .setValue(coords,value) + ajout .getValue(coords)

--- sudoku.py
+++ sudoku.py
@@ -1,13 +1,28 @@
+import random as rd
+
 class SudokuGrid:
     m = ["A","B","C","D","E","F","G","H","I"]
     n = ["1","2","3","4","5","6","7","8","9"]
 
-    def __init__(self, filler):
+    def __init__(self):
         # Structure de la grille : [[[[0,0,0],[0,0,0],[0,0,0]], [[0,0,0],[0,0,0],[0,0,0]], [[0,0,0],[0,0,0],[0,0,0]]],
         #                           [[[0,0,0],[0,0,0],[0,0,0]], [[0,0,0],[0,0,0],[0,0,0]], [[0,0,0],[0,0,0],[0,0,0]]],
         #                           [[[0,0,0],[0,0,0],[0,0,0]], [[0,0,0],[0,0,0],[0,0,0]], [[0,0,0],[0,0,0],[0,0,0]]]]
         # Traduction de la structure d'une grille par imbrication de listes en compréhension (NB : dimensions non sujettes à variation) :
-        self.grid = [[[[filler for _ in range(3)] for _ in range(3)] for _ in range(3)] for _ in range(3)]
+        self.grid = [[[[" " for _ in range(3)] for _ in range(3)] for _ in range(3)] for _ in range(3)]
+        
+        # Randomisation de la quantité de valeurs à préinsérer dans la grille :
+        fillersAmount = rd.randint(10,20)
+        for _ in range(fillersAmount):
+            # Randomisation du jeu de coordonnées pour chacune des valeurs à préinsérer :
+            fillerCoords = self.m[rd.randint(0,8)] + self.n[rd.randint(0,8)]
+            # NB : Génération aléatoire sans remise :
+            while self.getValue(fillerCoords) != " ":
+                fillerCoords = self.m[rd.randint(0,8)] + self.n[rd.randint(0,8)]
+            # Génération de la valeur en elle-même
+            fillerValue = str(rd.randint(1,9))
+            self.setValue(fillerCoords, fillerValue)
+        print(fillersAmount)
 
     def __str__(self):
         gridDisplay = "\n"
@@ -22,8 +37,29 @@ class SudokuGrid:
         # Nettoyage final de la chaine de caractères contenant la grille (remplacements chainés plus rapides) :
         gridDisplay = gridDisplay.replace("[","").replace("]","").replace(",","").replace("'","")
         return gridDisplay
-    
-    def insert(self, coords, value):
+
+    def getValue(self, coords):
+        coords = list(coords)
+        if len(coords) != 2:
+            print("Erreur : seulement 2 coordonnées à entrer")
+            return
+        elif (coords[0] not in self.m) or (coords[1] not in self.n):
+            print("Erreur : format des coordonnées non valide")
+            return
+        else:
+            # Accès type à une case : SudokuGrid.grid[rangée globale][colonne globale][rangée dans sous-grille][colonne dans sous-grille]
+            # coords[0] conditionne globalRow + subgridRow
+            indexM = self.m.index(coords[0])
+            globalRow = indexM // 3
+            subgridRow = indexM - 3 * (globalRow)
+            # coords[1] conditionne globalCol + subgridCol
+            indexN = self.n.index(coords[1])
+            globalCol = indexN // 3
+            subgridCol = indexN - 3 * (globalCol)
+
+            return self.grid[globalRow][globalCol][subgridRow][subgridCol]
+
+    def setValue(self, coords, value):
         coords = list(coords)
         if len(coords) != 2:
             print("Erreur : seulement 2 coordonnées à entrer")
@@ -32,7 +68,7 @@ class SudokuGrid:
             print("Erreur : format des coordonnées non valide")
             return
         elif value not in self.n:
-            print("Erreur : la valeur à insérer doit être un chiffre compris entre 1 et 9")
+            print("Erreur : la valeur à insérer doit être un entier compris entre 1 et 9")
             return
         else:
             # Accès type à une case : SudokuGrid.grid[rangée globale][colonne globale][rangée dans sous-grille][colonne dans sous-grille]
@@ -47,15 +83,19 @@ class SudokuGrid:
 
             self.grid[globalRow][globalCol][subgridRow][subgridCol] = value
 
-newGame = SudokuGrid("_")
-newGame.insert("F4", "9")
-newGame.insert("F8", "4")
-newGame.insert("G5", "3")
-newGame.insert("A2", "2")
-newGame.insert("H7", "5")
-newGame.insert("A1", "7")
-newGame.insert("I9", "4")
-newGame.insert("H8", "1")
-newGame.insert("C4", "2")
+    def isFillingValid(self):
+        pass
+
+
+newGame = SudokuGrid()
+# newGame.setValue("F4", "9")
+# newGame.setValue("F8", "4")
+# newGame.setValue("G5", "3")
+# newGame.setValue("A2", "2")
+# newGame.setValue("H7", "5")
+# newGame.setValue("A1", "7")
+# newGame.setValue("I9", "4")
+# newGame.setValue("H8", "1")
+# newGame.setValue("C4", "2")
 print(newGame.grid)
 print(newGame)
