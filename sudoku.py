9f1b1e449d35fe5859abd8a31a08e8ac232bf560
Luca Matarelli <luca.matarelli@gmail.com>
2022-05-10 23:19:50 +0200

    Modification SudokuGrid.__init__() : initialisation randomisée d'une grille de sudoku valide

--- sudoku.py
+++ sudoku.py
@@ -11,18 +11,25 @@ class SudokuGrid:
         # Traduction de la structure d'une grille par imbrication de listes en compréhension (NB : dimensions non sujettes à variation) :
         self.grid = [[[[" " for _ in range(3)] for _ in range(3)] for _ in range(3)] for _ in range(3)]
         
-        # Randomisation de la quantité de valeurs à préinsérer dans la grille :
+        # PROCESSUS DE GÉNÉRATION ALÉATOIRE
+        # Randomisation de la quantité (bornée) de valeurs à insérer initialement dans la grille :
         fillersAmount = rd.randint(10,20)
         for _ in range(fillersAmount):
             # Randomisation du jeu de coordonnées pour chacune des valeurs à préinsérer :
             fillerCoords = self.m[rd.randint(0,8)] + self.n[rd.randint(0,8)]
-            # NB : Génération aléatoire sans remise :
+            # NB : Génération aléatoire sans remise (= regénération si on tombe sur une case déjà remplie) :
             while self.getValue(fillerCoords) != " ":
                 fillerCoords = self.m[rd.randint(0,8)] + self.n[rd.randint(0,8)]
-            # Génération de la valeur en elle-même
+            # Randomisation de la valeur en elle-même :
             fillerValue = str(rd.randint(1,9))
+            # Garantie de validité de la grille initialement randomisée (= regénération si l'association coordonnées/valeur est interdite par les règles du sudoku) :
+            while not self.isFillerValid(fillerCoords, fillerValue):
+                fillerCoords = self.m[rd.randint(0,8)] + self.n[rd.randint(0,8)]
+                while self.getValue(fillerCoords) != " ":
+                    fillerCoords = self.m[rd.randint(0,8)] + self.n[rd.randint(0,8)]
+                fillerValue = str(rd.randint(1,9))
             self.setValue(fillerCoords, fillerValue)
-        print(fillersAmount)
+        print(f"\n({fillersAmount} valeurs préremplies)")
 
     def __str__(self):
         gridDisplay = "\n"
@@ -30,7 +37,7 @@ class SudokuGrid:
         gridDisplay += "  | 1 2 3 | 4 5 6 | 7 8 9\n"
         for i, gridRow in enumerate(self.grid):
             gridRowDisplay = ""
-            # Formatage des rangées (axe, sépérateurs et valeurs) :
+            # Formatage des rangées (axe, séparateurs et valeurs) :
             for j in range(3):
                 gridRowDisplay += self.m[(i*3)+j] + " | " + str(gridRow[0][j]) + " | " + str(gridRow[1][j]) + " | " + str(gridRow[2][j]) + "\n"
             gridDisplay += 25 * "-" + "\n" + str(gridRowDisplay)
@@ -84,10 +91,6 @@ class SudokuGrid:
             fillerCol = [self.grid[i][globalCol][j][subgridCol] for i in range(3) for j in range(3)]
             fillerSubgrid = sum(self.grid[globalRow][globalCol], [])
 
-            print(fillerRow)
-            print(fillerCol)
-            print(fillerSubgrid)
-            
             if (value in fillerRow) or (value in fillerCol) or (value in fillerSubgrid):
                 return False
             else:
