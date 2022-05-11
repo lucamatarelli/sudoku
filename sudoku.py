8aac5cc0e81accb4f6e321e6a352612aba0b1b22
Luca Matarelli <luca.matarelli@gmail.com>
2022-05-11 23:14:25 +0200

    Ajouts SudokuGrid.allCoords, isGridComplete(), .isGridValid() et .randomGenerate() + Modifications .setValue() et .isFillerValid() + Suppression .initFillersCoords + Nettoyages et arrangements

--- sudoku.py
+++ sudoku.py
@@ -1,50 +1,27 @@
 import random as rd
+from itertools import product
 
 class SudokuGrid:
     m = ("A","B","C","D","E","F","G","H","I")
     n = ("1","2","3","4","5","6","7","8","9")
+    allCoords = list(product(m, n))
 
     def __init__(self):
         # Structure de la grille : [[[[0,0,0],[0,0,0],[0,0,0]], [[0,0,0],[0,0,0],[0,0,0]], [[0,0,0],[0,0,0],[0,0,0]]],
         #                           [[[0,0,0],[0,0,0],[0,0,0]], [[0,0,0],[0,0,0],[0,0,0]], [[0,0,0],[0,0,0],[0,0,0]]],
         #                           [[[0,0,0],[0,0,0],[0,0,0]], [[0,0,0],[0,0,0],[0,0,0]], [[0,0,0],[0,0,0],[0,0,0]]]]
-        # Traduction de la structure d'une grille par imbrication de listes en compréhension (NB : dimensions non sujettes à variation) :
         self.grid = [[[[" " for _ in range(3)] for _ in range(3)] for _ in range(3)] for _ in range(3)]
-        self.initFillersCoords = ()
-        
-        # PROCESSUS DE GÉNÉRATION ALÉATOIRE
-        # Randomisation de la quantité (bornée) de valeurs à insérer initialement dans la grille :
-        fillersAmount = 25#rd.randint(20,25)
-        for _ in range(fillersAmount):
-            # Randomisation du jeu de coordonnées pour chacune des valeurs à préinsérer :
-            fillerCoords = rd.choice(self.m) + rd.choice(self.n)
-            # NB : Génération aléatoire sans remise (= regénération si on tombe sur une case déjà remplie) :
-            while self.getValue(fillerCoords) != " ":
-                fillerCoords = rd.choice(self.m) + rd.choice(self.n)
-            # Randomisation de la valeur en elle-même :
-            fillerValue = rd.choice(self.n)
-            # Garantie de validité de la grille initialement randomisée (= regénération si l'association coordonnées/valeur est interdite par les règles du sudoku) :
-            while not self.isFillerValid(fillerCoords, fillerValue):
-                fillerCoords = rd.choice(self.m) + rd.choice(self.n)
-                while self.getValue(fillerCoords) != " ":
-                    fillerCoords = rd.choice(self.m) + rd.choice(self.n)
-                fillerValue = rd.choice(self.n)
-            self.setValue(fillerCoords, fillerValue)
-            self.initFillersCoords += (fillerCoords,)
-        print(f"\n({fillersAmount} valeurs préremplies)")
+        self.randomGenerate() # Génération aléatoire d'une grille de sudoku remplie et valide
 
     def __str__(self):
         gridDisplay = "\n"
-        # Insertion de l'axe des colonnes :
-        gridDisplay += "  | 1 2 3 | 4 5 6 | 7 8 9\n"
+        gridDisplay += "  | 1 2 3 | 4 5 6 | 7 8 9\n" # Axe des colonnes
         for i, gridRow in enumerate(self.grid):
             gridRowDisplay = ""
-            # Formatage des rangées (axe, séparateurs et valeurs) :
             for j in range(3):
-                gridRowDisplay += self.m[(i*3)+j] + " | " + str(gridRow[0][j]) + " | " + str(gridRow[1][j]) + " | " + str(gridRow[2][j]) + "\n"
+                gridRowDisplay += self.m[(i*3)+j] + " | " + str(gridRow[0][j]) + " | " + str(gridRow[1][j]) + " | " + str(gridRow[2][j]) + "\n" # Axe des rangées et contenu
             gridDisplay += 25 * "-" + "\n" + str(gridRowDisplay)
-        # Nettoyage final de la chaine de caractères contenant la grille (remplacements chainés plus rapides) :
-        gridDisplay = gridDisplay.replace("[","").replace("]","").replace(",","").replace("'","")
+        gridDisplay = gridDisplay.replace("[","").replace("]","").replace(",","").replace("'","") # Nettoyage final
         return gridDisplay
 
     def getIndices(self, coords):
@@ -57,11 +34,11 @@ class SudokuGrid:
             return
         else:
             # Accès type à une case : SudokuGrid.grid[rangée globale][colonne globale][rangée dans sous-grille][colonne dans sous-grille]
-            # coords[0] conditionne globalRow + subgridRow
+            # coords[0] conditionne globalRow et subgridRow
             indexM = self.m.index(coords[0])
             globalRow = indexM // 3
             subgridRow = indexM - 3 * (globalRow)
-            # coords[1] conditionne globalCol + subgridCol
+            # coords[1] conditionne globalCol et subgridCol
             indexN = self.n.index(coords[1])
             globalCol = indexN // 3
             subgridCol = indexN - 3 * (globalCol)
@@ -75,32 +52,72 @@ class SudokuGrid:
             return self.grid[globalRow][globalCol][subgridRow][subgridCol]
 
     def setValue(self, coords, value):
+        if value not in (self.n + ("0",)):
+            print("Erreur : la valeur à insérer doit être un entier compris entre 1 et 9")
+            return
+        else:
+            indices = self.getIndices(coords)
+            if indices != None:
+                globalRow, globalCol, subgridRow, subgridCol = indices
+                if value == "0":
+                    self.grid[globalRow][globalCol][subgridRow][subgridCol] = " "
+                else:
+                    self.grid[globalRow][globalCol][subgridRow][subgridCol] = value
+
+    def isFillerValid(self, coords, value):
         if value not in self.n:
             print("Erreur : la valeur à insérer doit être un entier compris entre 1 et 9")
             return
         else:
             indices = self.getIndices(coords)
             if indices != None:
-                if coords in self.initFillersCoords:
-                    print("Erreur : vous ne pouvez pas modifier les valeurs initiales")
-                    return
+                globalRow, globalCol, subgridRow, subgridCol = indices
+
+                fillerRow = sum([self.grid[globalRow][i][subgridRow] for i in range(3)], [])
+                valueindexRow = self.n.index(coords[1])
+                del fillerRow[valueindexRow]
+
+                fillerCol = [self.grid[i][globalCol][j][subgridCol] for i in range(3) for j in range(3)]
+                valueindexCol = self.m.index(coords[0])
+                del fillerCol[valueindexCol]
+
+                fillerSubgrid = sum(self.grid[globalRow][globalCol], [])
+                valueIndexSubgrid = (valueindexCol % 3) * 3 + (valueindexRow % 3)
+                del fillerSubgrid[valueIndexSubgrid]
+
+                if (value in fillerRow) or (value in fillerCol) or (value in fillerSubgrid):
+                    return False
                 else:
-                    globalRow, globalCol, subgridRow, subgridCol = indices
-                    self.grid[globalRow][globalCol][subgridRow][subgridCol] = value
+                    return True
 
-    def isFillerValid(self, coords, value):
-        indices = self.getIndices(coords)
-        if indices != None:
-            globalRow, globalCol, subgridRow, subgridCol = indices
-
-            fillerRow = sum([self.grid[globalRow][i][subgridRow] for i in range(3)], [])
-            fillerCol = [self.grid[i][globalCol][j][subgridCol] for i in range(3) for j in range(3)]
-            fillerSubgrid = sum(self.grid[globalRow][globalCol], [])
-
-            if (value in fillerRow) or (value in fillerCol) or (value in fillerSubgrid):
+    def isGridComplete(self):
+        for coords in self.allCoords:
+            if self.getValue(coords) == " ":
                 return False
-            else:
-                return True
+        return True
+
+    def isGridValid(self):
+        for coords in self.allCoords:
+            currentValue = self.getValue(coords)
+            if not self.isFillerValid(coords, currentValue):
+                return (False, coords)
+        return True
+
+    def randomGenerate(self):
+        for coords in self.allCoords:
+            if self.getValue(coords) == " ":
+                possibleValues = rd.sample(self.n, 9)
+                for randomValue in possibleValues:
+                    if self.isFillerValid(coords, randomValue):
+                        self.setValue(coords, randomValue)
+                        if self.isGridComplete():
+                            return True
+                        else:
+                            if self.randomGenerate():
+                                return True
+                break
+        self.setValue(coords, "0")
+        return False
 
 
 game = SudokuGrid()
@@ -114,7 +131,7 @@ while playerAction != "3":
         playerCoords = input("À quel emplacement souhaitez-vous insérer un chiffre ? ")
         playerValue = input("Quel chiffre souhaitez-vous insérer ? ")
         game.setValue(playerCoords, playerValue)
-    if playerAction == "2":
+    elif playerAction == "2":
         resetConfirmation = input("Votre progression sera perdue. Entrez \"reset\" pour confirmer : ")
         if resetConfirmation == "reset":    
             game = SudokuGrid()
